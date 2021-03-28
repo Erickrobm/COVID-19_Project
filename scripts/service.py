5e8b2e97d0124907511b5c1c63edb33470327a31
Erickrobm <81532564+Erickrobm@users.noreply.github.com>
2021-03-28 14:57:25 -0600

    Add files via upload

---
+++ scripts/service.py
@@ -0,0 +1,99 @@
+# Importar librerias de Flask.
+from flask import Flask, request, jsonify, redirect
+from flask_cors import CORS
+
+# Importar librerias de Keras
+from keras.preprocessing import image
+
+# Importar las librerias de Python y cargar función "cargarModelo"
+# de script "model_loader"
+import numpy as np
+
+import requests
+import json
+import os
+import cv2
+from werkzeug.utils import secure_filename
+from model_loader import cargarModelo
+
+UPLOAD_FOLDER = '../images/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+port = int(os.getenv('PORT', 5000))
+print ("Port recognized: ", port)
+
+#Initialize the application service
+app = Flask(__name__)
+CORS(app)
+global loaded_model, graph
+loaded_model, graph = cargarModelo()
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+#Define a route
+@app.route('/')
+def main_page():
+	return '¡Servicio REST activo!'
+
+@app.route('/model/covid19/', methods=['GET','POST'])
+def default():
+    data = {"success": False}
+    if request.method == "POST":
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+        file = request.files['file']
+        # if user does not select file, browser also submit a empty part without filename
+        if file.filename == '':
+            print('No selected file')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            #loading image
+            filename = UPLOAD_FOLDER + '/' + filename
+            print("\nfilename:",filename)
+
+            WIDTH=64
+            HEIGHT=64
+            
+            image_to_predict = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+            image_to_predict = image_to_predict/255.
+            image_to_predict = cv2.resize(image, (WIDTH,HEIGHT))
+            image_to_predict = image_to_predict.reshape(-1,WIDTH,HEIGHT,1)
+            
+            with graph.as_default():
+            	result = loaded_model.predict(image_to_predict)
+            	# print(result)
+            	
+	# Resultados
+    if(result[0][0]>result[0][1] and result[0][0]>result[0][2]): 
+        prediction = 0
+
+    elif(result[0][1]>result[0][0] and result[0][1]>result[0][2]): 
+        prediction = 1    
+
+    else: prediction = 2
+                
+    CLASSES = ["NORMAL", "COVID-19", "Viral Pneumonia"]
+
+    ClassPred = CLASSES[prediction]
+    ClassProb = result
+            	
+    print("Diagnóstico del Paciente:", ClassPred)
+    print("Probabilidad: {:.2f}".format(ClassProb), "%")
+
+    #Results as Json
+    data["predictions"] = []
+    r = {"label": ClassPred, "score": float(ClassProb)}
+    data["predictions"].append(r)
+
+    #Success
+    data["success"] = True
+    
+    return jsonify(data)
+
+# Run de application
+app.run(host='0.0.0.0',port=port, threaded=False)
